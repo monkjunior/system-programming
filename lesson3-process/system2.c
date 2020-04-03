18ad94196b34d46fa2b3f58b7ae5fa0efe3adda0
vungocson <vungocson998@gmail.com>
2020-04-03 12:00:41 +0700

    Lesson3 Signal

--- lesson3-process/system2.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-
-int main(){
-    printf("Submit | VU NGOC SON | 20163580\n");
-    system("ps -aux &");
-    printf("This is my CPU info.\n\n");
-    exit(0);
-}
